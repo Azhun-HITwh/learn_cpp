6b87652ec16c384b13b455ca6164c890022d6d79
Adrian <zhuazhun1996@gmail.com>
2022-06-10 00:32:11 +0800

    2022-06-10 binary tree iterated recursively

--- src/binarytree.cpp
+++ src/binarytree.cpp
@@ -6,11 +6,11 @@ using namespace std;
 class BinaryTree
 {
 private:
-    cahr data;
+    char data;
     BinaryTree* lchild;
     BinaryTree* rchild;
 public:
-    BinaryTree(/* args */);
+    BinaryTree();
     ~BinaryTree();
     
     BinaryTree* Create_BinaryTree();
@@ -26,10 +26,68 @@ public:
     int getBinaryTreeHeight(BinaryTree* T);
 };
 
-BinaryTree::BinaryTree(/* args */)
+BinaryTree::BinaryTree()
 {
 }
 
 BinaryTree::~BinaryTree()
 {
+}
+
+BinaryTree* BinaryTree::Create_BinaryTree(){
+    BinaryTree* T = new BinaryTree;
+    char ch;
+    scanf("%c", &ch);
+    if (ch == '#')
+    {
+        T = NULL;
+    }else{
+        T->data = ch;
+        T->lchild = Create_BinaryTree();
+        T->rchild = Create_BinaryTree();
+    }
+    return T;
+}
+
+void BinaryTree::PreorderTraversal(BinaryTree* T){
+    if(T == NULL)
+    {
+        return;
+    }
+    printf("%c ", T->data);
+    T->PreorderTraversal(T->lchild);
+    T->PreorderTraversal(T->rchild);
+}
+
+void BinaryTree::InorderTraversal(BinaryTree* T){
+    if (T == NULL)
+    {
+        return;
+    }
+    T->InorderTraversal(T->lchild);
+    printf("%c ", T->data);
+    T->InorderTraversal(T->rchild);
+}
+
+void BinaryTree::PostorderTraversal(BinaryTree* T){
+    if (T == NULL)
+    {
+        return;
+    }
+    T->PostorderTraversal(T->lchild);
+    T->PostorderTraversal(T->rchild);
+    printf("%c ", T->data);
+}
+
+int main(int argc, char const *argv[])
+{
+    cout<<"请初始化二叉树:"<<endl;
+    BinaryTree* T;
+    T = T->Create_BinaryTree();
+
+    cout<<"前序遍历（递归）："<<endl;
+    T->PreorderTraversal(T);
+    cout<<endl;
+
+    return 0;
 }
